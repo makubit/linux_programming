afbfc2ef58e8a26354ed3b0bc4fac97f9d7a3822
Magdalena Kubit <kubit.magdalena@gmail.com>
2018-11-18 20:06:22 +0100

    dodanie programu wspomagającego do generatora danych losowych

---
+++ FILE IO/generator_danych_losowych/rnd_doubles_idx.c
@@ -0,0 +1,16 @@
+//program generuje dane za pomocą innych programów, następnie uruchamia potrzebną liczbę współbieżnych z nim procesów, z których każdy wykonuje program dbl_index
+//
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+int main(int argc, char* argv[])
+{
+	
+
+	return 0;
+}
