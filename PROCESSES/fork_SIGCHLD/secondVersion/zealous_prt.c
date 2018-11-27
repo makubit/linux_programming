74322dbdb7628783860d72907432c1eff82585d9
Magdalena Kubit <kubit.magdalena@gmail.com>
2018-11-27 02:42:41 +0100

    puh

--- PROCESSES/fork_SIGCHLD/secondVersion/zealous_prt.c
+++ PROCESSES/fork_SIGCHLD/secondVersion/zealous_prt.c
@@ -11,6 +11,9 @@
 #include <sys/wait.h>
 #include <math.h>
 
+/* Define flags */
+#define NOMORECHLD FALSE
+
 /* Define child Queue*/
 struct chldQueue
 {
@@ -31,7 +34,11 @@ struct chldQueue* queue = NULL;
 struct chldQueue* newItem = NULL;
 struct chldQueue* first  = NULL;
 
-int childrenNumber = 0;
+static volatile int childrenNumber = 0;
+static volatile int childrenNumberDeadOrAlive = 0;
+static int pgid;
+/* Flag to set when there are no more alive children */
+//int flag = 0;
 
 /* Define main functions to handle queue */
 /*void push(siginfo_t status)
@@ -53,6 +60,7 @@ int childrenNumber = 0;
 	(*queue).pTail = new_item;
 }*/
 
+/* Functions to manage queue */
 void push(siginfo_t status)
 {
 	newItem = (struct chldQueue*)malloc(sizeof(struct chldQueue));
@@ -64,28 +72,81 @@ void push(siginfo_t status)
 	queue = newItem;
 }
 
+void pop()
+{
+	if(first != NULL)
+	{
+		newItem = (*first).next;
+
+		/* Print obituary */
+		printf("\n----------\nCHILD HAS JUST DIEDED\nit's PID: %d\nstatus: %d\ncause of death: %d\n[*] REQUIESCAT IN PACE [*]\n\n", first->status.si_pid, first->status.si_status, first->status.si_code);
+		
+		free(first);
+		first = newItem;
+		if(first == NULL)
+				  queue = NULL;
+	}
+	else
+			  printf("Queue is empty, cannot pop...\n");
+}
+
 
 /* Definfe function to handle signal SIGCHLD */
-static void sigchld_handler(int signo, siginfo_t* siginfo, void* context)
+static void sigchld_handler(int signo, siginfo_t* status, void* context)
 {
 	//printf("Jestem w obsłusze syngału!\nLiczba dzieci: %d\n pid: %d, %d\n", childrenNumber, status.si_pid, status.si_code);
+	//printf("Pn");
 
-	if(siginfo->si_code == CLD_KILLED ) /* Write to queue; 2 == CLDKILLED*/
+	int status2;
+	pid_t childPid;
+	printf("--->%d<----%d::%d\n\n%d\n\n",status->si_pid, getpgid(status->si_pid), pgid, status->si_code);
+	while(waitpid(-1, &status2, WNOHANG) > 0 )
+	//while((status->si_pid > 0) && (getsid(status->si_pid) == pgid))
+	//while(childPid = getpid() > 0)
 	{
-		//push(status);
-		printf("--->------>Jestem w obsłusze syngału!\nLiczba dzieci: %d\n pid: %d\n", childrenNumber, siginfo->si_pid);
+		//printf("Obsługa sygnału góra\t");
+		//if( siginfo->si_code == CLD_KILLED ) /* Write to queue; 2 == CLDKILLED*/
+		if(status->si_code == CLD_KILLED)
+		{
+			push(*status); /* Push new obituary when is dead */ //???????????????????
+			printf("--->------>Jestem w obsłusze syngału!\nLiczba dzieci: %d\n pid: %d\n", childrenNumber, status->si_pid); /* Nie powinno się tego tak robić */
 
-		childrenNumber--;
+			childrenNumber--;
+			childrenNumberDeadOrAlive--;
+		}
+
+		else if(status->si_code == CLD_STOPPED)
+		{childrenNumberDeadOrAlive--;
+		printf("Obsługa sygnału stopped\t");
+
+		}
+
+		else if(status->si_code == CLD_CONTINUED)
+		{
+			childrenNumberDeadOrAlive++;
+			printf("Obsługa sygnału continued\t%d", childrenNumberDeadOrAlive);
+
+		}
+
+		else if(childrenNumber == 0)
+				  return;
+
+
+	// ??????????????????????
+	/* jeśli flaga continued, to ++
+	 * jeśli flaga stopped to --
+	 */
+
 	}
 
 	//if(childrenNumber == 0)
 	//{
 		//wypisuje flagę -> ALE GDZIE???
+		//flag = NOMORECHLD;
 	//}		
 }
 
 //----------------------------------------------------
-
 int main(int argc, char* argv[])
 {
 	int c;
@@ -105,7 +166,7 @@ int main(int argc, char* argv[])
 				break;
 		}
 
-	int pgid = 0;
+	//int pgid = 0;
 	//int childrenNumber = 0;
 	
 	/* Check if there are two positional parameters */
@@ -124,6 +185,7 @@ int main(int argc, char* argv[])
 	}
 
 	childrenNumber = strtol(argv[optind+1], &pEnd, 0);
+	childrenNumberDeadOrAlive = childrenNumber;
 	if(*pEnd)
 	{
 		printf("Parameter has to be a int number, try again...\n");
@@ -151,30 +213,58 @@ int main(int argc, char* argv[])
 
 	/* Create sigaction structure to monitor incoming signals */
 	struct sigaction act;
+	//act.sa_handler = sigchld_handler;
+	//sigemptyset(act.sa_mask);
+	//act.sa_handler = sigchld_handler;
 	act.sa_sigaction = &sigchld_handler;
 	act.sa_flags = SA_SIGINFO;
+	sigset_t emptyMask, blockMask;
 
-	
+	/* Handle SIGCHLD signal */
+	if(sigaction(SIGCHLD, &act, NULL) < 0)
+	{
+		fprintf(stderr, "Cannot handle signal SIGCHLD\n");
+		return -1;
+	}
+	//sigemptyset(&blockMask);
+	//sigaddset(&blockMask, SIGCHLD);
+	//sigprocmask(SIG_SETMASK, &blockMask, NULL);
+
+	/* Wake up whole group of children */
+	//ustawić if childrenNumber == 0 ????????????????
+	int sendContinue = killpg(pgid, SIGCONT);
+	if(sendContinue)
+		printf("Something went wrong / Sending SIGCONT\n");
+
+	sigemptyset(&emptyMask);
 	/* Until all children die */
 	while(childrenNumber > 0)
 	{ 
-		/* Handle SIGCHLD signal */
-		if(status.si_pid > 0)
-			if(sigaction(SIGCHLD, &act, NULL) < 0)
-			{
-				fprintf(stderr, "Cannot handle signal SIGCHLD\n");
-				return -1;
-			}
+		/* Passive waiting */
+		pause();
 
-		/* Wake up whole group of children */
-		int sendContinue = killpg(pgid, SIGCONT);
-		if(sendContinue)
-			printf("Something went wrong / Sending SIGCONT\n");
-		
-		/* Passive waiting ??????? */
-	
-		/* Print which Child has died */	
+		/* Print which Child has died -> next loop */
+		while(queue)
+			pop();
 
+		/* if no more alive and -> managed by while loop */
+		/* if no more active chld -> childrenNumber == 0 -> werid nanosleep, then wake up chld */
+		if((childrenNumberDeadOrAlive == 0) && (childrenNumber > 0))
+		{
+			printf("!!!!!!!!!!!Sleeping...\n");
+			sleep(2);
+
+			/* Wake up all children */
+			int sendContinue = killpg(pgid, SIGCONT);
+			if(sendContinue)
+				printf("Something went wrong / Sending SIGCONT\n");
+		}
+
+		/* if chl > 0 continue while loop */ 
+		printf("Waiting....\n");
+		//sleep(5);
+
+		printf("%d<<>>%d", childrenNumber, childrenNumberDeadOrAlive);
 	}
 
 
