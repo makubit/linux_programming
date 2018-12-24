29205e4f4530996e644ed2787ae32992debc5987
Magdalena Kubit <kubit.magdalena@gmail.com>
2018-12-24 01:31:26 +0100

    added fork loop

--- FIFO/zad3/multich_tr2.c
+++ FIFO/zad3/multich_tr2.c
@@ -2,6 +2,7 @@
 //MULTICHANNEL FILTER
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -16,151 +17,156 @@
 
 int main(int argc, char* argv[])
 {
-   int c;
-   char characters[9];
-   char* fileWithData = NULL;
-   while((c = getopt(argc, argv, "X:f:")) != -1)
-      switch(c)
-      {
-         case 'X':
-            //strcpy(characters, optarg);
-            /*if(!optarg[10])
+    //printf("%d, %d, %d, %d\n\n", POLLERR, POLLHUP, POLLNVAL);
+    int c;
+    char characters[11];
+    char* fileWithData = NULL;
+    while((c = getopt(argc, argv, "X:f:")) != -1)
+        switch(c)
+        {
+            case 'X':
+                /* Take only 10 characters from parameter */
+                for(int i = 0; i< 10; i++)
+                    characters[i] = optarg[i];
+                characters[10] = '\0'; //mark end of string
+
+                //printf("%s, %c\n", characters, optarg[9]);
+                break;
+            case 'f':
+                fileWithData = optarg;
+                break;
+            case '?':
+                perror("Wrong usage of parameters\n");
+                break;
+        }
+
+    if(characters == NULL)
+    {
+        perror("Parameter -X is mandatory, exiting...\n");
+        return 1;
+    }
+
+    /* Prepare pipes */
+    int fd1[2]; //sends input data from parent to child
+    int fd2[2]; //sends edited string from child to parent
+    //int fdData;
+
+    int fd_data = open(fileWithData, O_RDONLY);
+    for(int i = 0; i<strlen(characters); i++)
+    {
+//        int fd_data = open(fileWithData, O_RDONLY);
+
+        if(pipe(fd1) == -1)
+        {
+            perror("Pipe failed, exiting...\n");
+            return 1;
+        }
+
+        if(pipe(fd2) == -1)
+        {
+            perror("Pipe failed, exiting...\n");
+            return 1;
+        }
+
+        //TODO: while for children
+        printf("NOW PROCESSING CHILD: %c\n", characters[i]);
+
+        //make a child which will execute tr -d 'letter'
+        pid_t pid = fork();
+        if(pid < 0)
+        {
+            perror("Child failed\n");
+            return 1;
+        }
+
+        if(pid == 0) //child
+        {
+            close(fd1[1]); //writing end
+
+            /* Read string using first pipe */
+            char buff[100];
+            read(fd1[0], buff, 100);
+            printf("%s--> buff inside child\n", buff);
+
+            //NEXT STEP: execute tr -d !!!!!!!!!!
+            int fd_tr1[2];
+            pipe(fd_tr1);
+            int fd_tr2[2];
+            pipe(fd_tr2);
+
+            pid_t chld_pid = fork();
+            if(chld_pid == 0) //child execute tr -d
             {
-               printf("String is too long -> max 10 characters\n");
-               return 1;
-            }*/
-            //strcpy(characters, optarg);
-            for(int i = 0; i< 10; i++)
-               characters[i] = optarg[i];
-            
-            printf("%s, %c\n", characters, optarg[10]);
-            break;
-         case 'f':
-            fileWithData = optarg;
-            break;
-         case '?':
-            perror("Wrong usage of parameters\n");
-         break;
-      }
+                /* Close all unused descriptors, than replace fd_tr1[1] with stdin and fd_tr2[0] with stdout */
+                close(fd_tr1[1]);
+                close(fd_tr2[0]);
 
-   if(characters == NULL)
-   {
-      perror("Parameter -X is mandatory, exiting...\n");
-      return 1;
-   }
+                dup2(fd_tr1[0], 0); //replace stdin with input end of pipe
+                dup2(fd_tr2[1], 1); //replace stdout with output end of pipe
 
-   /* Prepare pipes */
-   int fd1[2]; //sends input data from parent to child
-   int fd2[2]; //sends edited string from child to parent
-   //int fdData;
+                close(fd_tr1[0]);
+                close(fd_tr2[1]);
 
-   int fd_data = open(fileWithData, O_RDONLY);
+                char c[2] = { characters[i], '\0'};
+                execl("/usr/bin/tr", "/usr/bin/tr", "-d", c, NULL);
 
-   if(pipe(fd1) == -1)
-   {
-      perror("Pipe failed, exiting...\n");
-      return 1;
-   }
+                exit(0);
+            }
 
-   if(pipe(fd2) == -1)
-   {
-      perror("Pipe failed, exiting...\n");
-      return 1;
-   }
+            else //parent writes and reads from pipes
+            {
+                read(fd_data, buff, 100);
+                printf("PARENT CHILD %s", buff);
+                close(fd_tr1[0]);
+                close(fd_tr2[1]);
 
-   //make a child which will execute tr -d 'letter'
-   pid_t pid = fork();
-   if(pid < 0)
-   {
-      perror("Child failed\n");
-      return 1;
-   }
+                write(fd_tr1[1], buff, sizeof(buff));
+                close(fd_tr1[1]);
 
-   if(pid == 0) //child
-   {
-      close(fd1[1]); //writing end
+                read(fd_tr2[0], buff, sizeof(buff));
+                close(fd_tr2[0]);
 
-      /* Read string using first pipe */
-      char buff[100];
-      read(fd1[0], buff, 100);
-      printf("%s--> buff inside child\n", buff);
+            }
 
-      //NEXT STEP: execute tr -d !!!!!!!!!!
-      int fd_tr1[2];
-      pipe(fd_tr1);
-      int fd_tr2[2];
-      pipe(fd_tr2);
+            close(fd1[0]);
+            close(fd2[0]);
 
-      pid_t chld_pid = fork();
-      if(chld_pid == 0)
-      {
-          /* Close all unused descriptors, than replace fd_tr1[1] with stdin and fd_tr2[0] with stdout */
-          close(fd_tr1[1]);
-          close(fd_tr2[0]);
-          
-          dup2(fd_tr1[0], 0); //replace stdin with input end of pipe
-          dup2(fd_tr2[1], 1); //replace stdout with output end of pipe
-          
-          close(fd_tr1[0]);
-          close(fd_tr2[1]);
+            /* Write result to pipe */
+            write(fd2[1], buff, 100);
+            close(fd2[1]);
 
-          execl("/usr/bin/tr", "/usr/bin/tr", "-d", "a", NULL);
-          
-          exit(0);
-      }
-      else
-      {
-          read(fd_data, buff, 100);
-          //printf("%s", buff);
-          close(fd_tr1[0]);
-          close(fd_tr2[1]);
+            exit(0);
+        }
 
-          write(fd_tr1[1], buff, sizeof(buff));
-          close(fd_tr1[1]);
+        else //parent
+        {
+            close(fd1[0]);
 
-          read(fd_tr2[0], buff, sizeof(buff));
-          close(fd_tr2[0]);
+            char buff[100];
 
-      }
+            /* Write string to process in child */
+            write(fd1[1], buff, 100);
+            close(fd1[1]);
 
-      close(fd1[0]);
-      close(fd2[0]);
+            printf("Waiting for child to process...\n");
 
-      /* Write result to pipe */
-      write(fd2[1], buff, 100);
-      close(fd2[1]);
+            struct pollfd pollfds;
+            pollfds.fd = fd2[1];
+            pollfds.events = POLLOUT;
 
-      return 0;
-   }
-   else //parent
-   {
-       close(fd1[0]);
+            poll(&pollfds, 1, 500);	
 
-       char buff[100] = "Random Dataaaaaeeee eee";
+            printf("events: %d, revents: %d\n\n",pollfds.events, pollfds.revents);
 
-       /* Write string to process in child */
-       write(fd1[1], buff, 100);
-       close(fd1[1]);
+            close(fd2[1]);
 
-       printf("Waiting for child to process...\n");
+            /* Read processed string from second pipe */
+            char buff2[100];
+            read(fd2[0], buff2, 100);
+            printf("PROCESSED STRING RESULT: %s\n\n", buff2);
 
-       struct pollfd pollfds;
-       pollfds.fd = fd2[1];
-       pollfds.events = POLLOUT;
-
-       poll(&pollfds, 1, 500);	
-
-       printf("events: %d, revents: %d\n\n",pollfds.events, pollfds.revents);
-
-       close(fd2[1]);
-
-       /* Read processed string from second pipe */
-       char buff2[100];
-       read(fd2[0], buff2, 100);
-       printf("PROCESSED STRING RESULT: %s\n\n", buff2);
-
-       close(fd2[0]);
-   }
-
-   return 0;
+            close(fd2[0]);
+        }
+    }
+    return 0;
 }
