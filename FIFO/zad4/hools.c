38d3d13217557df9a28258ed08a7faf81b78ae43
Magdalena Kubit <kubit.magdalena@gmail.com>
2019-01-04 00:29:09 +0100

    add4th task

---
+++ FIFO/zad4/hools.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+
+int main(int argc, char* argv[])
+{
+    
+
+    return 0;
+}
